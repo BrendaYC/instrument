79d754b50d148ba46488358f9de113f554117af6
BrendaYC <44791357+BrendaYC@users.noreply.github.com>
2019-09-10 20:30:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
   author = 'ETCGroup',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/BrendaYC/instrument',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/BrendaYC/instrument/archive/'0.1'.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/BrendaYC/instrument/archive/'0.1'.tar.gz',   
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[      
+          'pandas',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
